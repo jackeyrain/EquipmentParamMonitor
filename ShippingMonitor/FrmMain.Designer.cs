d6a4fc53e67e07b27422d424f3262fa47b75d98d
jackeyrain <45423576+jackeyrain@users.noreply.github.com>
2020-11-17 03:08:08 +0800

    加入shipping监控程序

---
+++ ShippingMonitor/FrmMain.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.GROUP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SHIPPING_CODE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.STATUS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.COUNT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SEQ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QTY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FROM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DELT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.GROUP,
+            this.SHIPPING_CODE,
+            this.STATUS,
+            this.COUNT,
+            this.SEQ,
+            this.QTY,
+            this.FROM,
+            this.TO,
+            this.DELT});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(867, 498);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Text = "dataGridView1";
+            this.dataGridView1.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView1_DataBindingComplete);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // GROUP
+            // 
+            this.GROUP.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.GROUP.DataPropertyName = "GROUP";
+            this.GROUP.HeaderText = "GROUP";
+            this.GROUP.MinimumWidth = 6;
+            this.GROUP.Name = "GROUP";
+            this.GROUP.ReadOnly = true;
+            this.GROUP.Width = 91;
+            // 
+            // SHIPPING_CODE
+            // 
+            this.SHIPPING_CODE.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.SHIPPING_CODE.DataPropertyName = "SHIPPING_CODE";
+            this.SHIPPING_CODE.HeaderText = "SHIPPING_CODE";
+            this.SHIPPING_CODE.MinimumWidth = 6;
+            this.SHIPPING_CODE.Name = "SHIPPING_CODE";
+            this.SHIPPING_CODE.ReadOnly = true;
+            this.SHIPPING_CODE.Width = 156;
+            // 
+            // STATUS
+            // 
+            this.STATUS.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.STATUS.DataPropertyName = "STATUS";
+            this.STATUS.HeaderText = "STATUS";
+            this.STATUS.MinimumWidth = 6;
+            this.STATUS.Name = "STATUS";
+            this.STATUS.ReadOnly = true;
+            this.STATUS.Width = 96;
+            // 
+            // COUNT
+            // 
+            this.COUNT.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.COUNT.DataPropertyName = "COUNT";
+            this.COUNT.HeaderText = "COUNT";
+            this.COUNT.MinimumWidth = 6;
+            this.COUNT.Name = "COUNT";
+            this.COUNT.ReadOnly = true;
+            this.COUNT.Width = 92;
+            // 
+            // SEQ
+            // 
+            this.SEQ.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.SEQ.DataPropertyName = "SEQ";
+            this.SEQ.HeaderText = "SEQ";
+            this.SEQ.MinimumWidth = 6;
+            this.SEQ.Name = "SEQ";
+            this.SEQ.ReadOnly = true;
+            this.SEQ.Width = 67;
+            // 
+            // QTY
+            // 
+            this.QTY.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.QTY.DataPropertyName = "QTY";
+            this.QTY.HeaderText = "QTY";
+            this.QTY.MinimumWidth = 6;
+            this.QTY.Name = "QTY";
+            this.QTY.ReadOnly = true;
+            this.QTY.Width = 68;
+            // 
+            // FROM
+            // 
+            this.FROM.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.FROM.DataPropertyName = "FROM";
+            this.FROM.HeaderText = "FROM";
+            this.FROM.MinimumWidth = 6;
+            this.FROM.Name = "FROM";
+            this.FROM.ReadOnly = true;
+            this.FROM.Width = 83;
+            // 
+            // TO
+            // 
+            this.TO.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.TO.DataPropertyName = "TO";
+            this.TO.HeaderText = "TO";
+            this.TO.MinimumWidth = 6;
+            this.TO.Name = "TO";
+            this.TO.ReadOnly = true;
+            this.TO.Width = 59;
+            // 
+            // DELT
+            // 
+            this.DELT.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.DELT.DataPropertyName = "DELT";
+            this.DELT.HeaderText = "DELT";
+            this.DELT.MinimumWidth = 6;
+            this.DELT.Name = "DELT";
+            this.DELT.ReadOnly = true;
+            this.DELT.Width = 74;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 498);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GROUP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SHIPPING_CODE;
+        private System.Windows.Forms.DataGridViewTextBoxColumn STATUS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn COUNT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SEQ;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QTY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FROM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DELT;
+    }
+}
+
---
+++ ShippingMonitor/FrmMain.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmMain : Form
+    {
+        IFreeSql fsql = null;
+        public FrmMain()
+        {
+            InitializeComponent();
+            dataGridView1.AutoGenerateColumns = false;
+        }
+
+        string sql = @"SELECT P.* from ( 
+                        SELECT 'IP SHIPPING' 'GROUP', p.SHIPPING_CODE, p.STATUS, sum(p.COUNT) 'COUNT', p.SEQ, COUNT(1) 'QTY', MIN(p.CUST_INFO_SEQ) 'FROM', max(p.CUST_INFO_SEQ) 'TO', (max(p.CUST_INFO_SEQ) - MIN(p.CUST_INFO_SEQ)) / 10 + 1 'DELT'  from (
+                        SELECT a.SHIPPING_CODE, a.SEQ, b.CUST_INFO_SEQ, a.STATUS, COUNT(b.CUST_INFO_SEQ) 'COUNT' From MES.TT_WM_SHIPPING a inner join MES.TT_WM_SHIPPING_DETAIL b on a.FID = b.SHIPPING_FID
+                          where a.PART_SHIPPING_FID = '7b3a5c96-5f2e-428a-b8f7-42f02c83222d' and a.CREATE_DATE > DATEADD(DAY, -1, GETDATE()) 
+                          group by a.SHIPPING_CODE, a.SEQ, b.CUST_INFO_SEQ, b.CUST_INFO_SEQ, a.STATUS ) as p
+                          group by p.SHIPPING_CODE, p.SEQ, p.STATUS
+                        UNION all
+                        SELECT 'LEFT DP SHIPPING' 'GROUP', p.SHIPPING_CODE, p.STATUS, sum(p.COUNT) 'COUNT', p.SEQ, COUNT(1) 'QTY', MIN(p.CUST_INFO_SEQ) 'FROM', max(p.CUST_INFO_SEQ) 'TO', (max(p.CUST_INFO_SEQ) - MIN(p.CUST_INFO_SEQ)) / 10 + 1 'DELT' from (
+                        SELECT a.SHIPPING_CODE, a.SEQ, b.CUST_INFO_SEQ, a.STATUS, COUNT(b.CUST_INFO_SEQ) 'COUNT' From MES.TT_WM_SHIPPING a inner join MES.TT_WM_SHIPPING_DETAIL b on a.FID = b.SHIPPING_FID
+                          where a.PART_SHIPPING_FID = '0d985300-04b1-423c-b769-1ec765bc4430' and a.CREATE_DATE > DATEADD(DAY, -1, GETDATE()) 
+                          group by a.SHIPPING_CODE, a.SEQ, b.CUST_INFO_SEQ, b.CUST_INFO_SEQ, a.STATUS ) as p
+                          group by p.SHIPPING_CODE, p.SEQ, p.STATUS
+                        UNION all
+                        SELECT 'RIGHT DP SHIPPING' 'GROUP', p.SHIPPING_CODE, p.STATUS, sum(p.COUNT) 'COUNT', p.SEQ, COUNT(1) 'QTY', MIN(p.CUST_INFO_SEQ) 'FROM', max(p.CUST_INFO_SEQ) 'TO', (max(p.CUST_INFO_SEQ) - MIN(p.CUST_INFO_SEQ)) / 10 + 1 'DELT' from (
+                        SELECT a.SHIPPING_CODE, a.SEQ, b.CUST_INFO_SEQ, a.STATUS, COUNT(b.CUST_INFO_SEQ) 'COUNT' From MES.TT_WM_SHIPPING a inner join MES.TT_WM_SHIPPING_DETAIL b on a.FID = b.SHIPPING_FID
+                          where a.PART_SHIPPING_FID = '6fd55e7e-d41a-45c6-945a-78c847fa75ba' and a.CREATE_DATE > DATEADD(DAY, -1, GETDATE()) 
+                          group by a.SHIPPING_CODE, a.SEQ, b.CUST_INFO_SEQ, b.CUST_INFO_SEQ, a.STATUS ) as p
+                          group by p.SHIPPING_CODE, p.SEQ, p.STATUS
+                        UNION all
+                        SELECT 'CONSOLE SHIPPING DS' 'GROUP', p.SHIPPING_CODE, p.STATUS, sum(p.COUNT) 'COUNT', p.SEQ, COUNT(1) 'QTY', MIN(p.CUST_INFO_SEQ) 'FROM', max(p.CUST_INFO_SEQ) 'TO', (max(p.CUST_INFO_SEQ) - MIN(p.CUST_INFO_SEQ)) / 10 + 1'DELT' from (
+                        SELECT a.SHIPPING_CODE, a.SEQ, b.CUST_INFO_SEQ, a.STATUS, COUNT(b.CUST_INFO_SEQ) 'COUNT' From MES.TT_WM_SHIPPING a inner join MES.TT_WM_SHIPPING_DETAIL b on a.FID = b.SHIPPING_FID
+                          where a.PART_SHIPPING_FID = '8f522937-1733-46d5-8941-f79725ab9aaf' and a.CREATE_DATE > DATEADD(DAY, -1, GETDATE()) 
+                          group by a.SHIPPING_CODE, a.SEQ, b.CUST_INFO_SEQ, b.CUST_INFO_SEQ, a.STATUS ) as p
+                          group by p.SHIPPING_CODE, p.SEQ, p.STATUS
+                        UNION all
+                        SELECT 'CONSOLE SHIPPING WS' 'GROUP', p.SHIPPING_CODE, p.STATUS, sum(p.COUNT) 'COUNT', p.SEQ, COUNT(1) 'QTY', MIN(p.CUST_INFO_SEQ) 'FROM', max(p.CUST_INFO_SEQ) 'TO', (max(p.CUST_INFO_SEQ) - MIN(p.CUST_INFO_SEQ)) / 10 + 1'DELT' from (
+                        SELECT a.SHIPPING_CODE, a.SEQ, b.CUST_INFO_SEQ, a.STATUS, COUNT(b.CUST_INFO_SEQ) 'COUNT' From MES.TT_WM_SHIPPING a inner join MES.TT_WM_SHIPPING_DETAIL b on a.FID = b.SHIPPING_FID
+                          where a.PART_SHIPPING_FID = 'a79cc57d-011f-4890-9097-9aca05e91c5e' and a.CREATE_DATE > DATEADD(DAY, -1, GETDATE()) 
+                          group by a.SHIPPING_CODE, a.SEQ, b.CUST_INFO_SEQ, b.CUST_INFO_SEQ, a.STATUS ) as p
+                          group by p.SHIPPING_CODE, p.SEQ, p.STATUS
+                          ) as P
+                          order by P.[GROUP], P.SEQ";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fsql = new FreeSql.FreeSqlBuilder()
+                .UseConnectionString(FreeSql.DataType.SqlServer, "Data Source=10.16.112.21;Initial Catalog=Pisces;User ID=pisces_user;Password=sahasnopassword")
+                .Build();
+            timer1.Enabled = true;
+            timer1_Tick(null, null);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var table = fsql.Ado.ExecuteDataTable(sql);
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = table;
+            dataGridView1.Refresh();
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            Boolean result = true;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value.ToString().Equals("IP SHIPPING", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (row.Cells["QTY"].Value.ToString().Equals("3") &&
+                        row.Cells["DELT"].Value.ToString().Equals("3"))
+                    {
+                        row.DefaultCellStyle.BackColor = Color.GreenYellow;
+                    }
+                    else
+                    {
+                        row.DefaultCellStyle.BackColor = Color.OrangeRed;
+                        result = false;
+                    }
+                }
+                else if (row.Cells[0].Value.ToString().Equals("LEFT DP SHIPPING", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (row.Cells["QTY"].Value.ToString().Equals("18") &&
+                       row.Cells["DELT"].Value.ToString().Equals("18") &&
+                       row.Cells["COUNT"].Value.ToString().Equals("36"))
+                    {
+                        row.DefaultCellStyle.BackColor = Color.GreenYellow;
+                    }
+                    else
+                    {
+                        row.DefaultCellStyle.BackColor = Color.OrangeRed;
+                        result = false;
+                    }
+                }
+                else if (row.Cells[0].Value.ToString().Equals("RIGHT DP SHIPPING", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (row.Cells["QTY"].Value.ToString().Equals("18") &&
+                       row.Cells["DELT"].Value.ToString().Equals("18") &&
+                       row.Cells["COUNT"].Value.ToString().Equals("36"))
+                    {
+                        row.DefaultCellStyle.BackColor = Color.GreenYellow;
+                    }
+                    else
+                    {
+                        row.DefaultCellStyle.BackColor = Color.OrangeRed;
+                        result = false;
+                    }
+                }
+            }
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value.ToString().Equals("IP SHIPPING") &&
+                    i + 1 <= dataGridView1.Rows.Count - 1 &&
+                    dataGridView1.Rows[i + 1].Cells[0].Value.ToString().Equals("IP SHIPPING"))
+                {
+                    if (int.Parse(dataGridView1.Rows[i + 1].Cells["FROM"].Value.ToString()) -
+                        int.Parse(dataGridView1.Rows[i].Cells["TO"].Value.ToString()) == 10)
+                    {
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.GreenYellow;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.OrangeRed;
+                        result = false;
+                    }
+                }
+
+                if (dataGridView1.Rows[i].Cells[0].Value.ToString().Equals("LEFT DP SHIPPING") &&
+                    i + 1 <= dataGridView1.Rows.Count - 1 &&
+                    dataGridView1.Rows[i + 1].Cells[0].Value.ToString().Equals("LEFT DP SHIPPING"))
+                {
+                    if (int.Parse(dataGridView1.Rows[i + 1].Cells["FROM"].Value.ToString()) -
+                        int.Parse(dataGridView1.Rows[i].Cells["TO"].Value.ToString()) == 10)
+                    {
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.GreenYellow;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.OrangeRed;
+                        result = false;
+                    }
+                }
+
+                if (dataGridView1.Rows[i].Cells[0].Value.ToString().Equals("RIGHT DP SHIPPING") &&
+                  i + 1 <= dataGridView1.Rows.Count - 1 &&
+                  dataGridView1.Rows[i + 1].Cells[0].Value.ToString().Equals("RIGHT DP SHIPPING"))
+                {
+                    if (int.Parse(dataGridView1.Rows[i + 1].Cells["FROM"].Value.ToString()) -
+                        int.Parse(dataGridView1.Rows[i].Cells["TO"].Value.ToString()) == 10)
+                    {
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.GreenYellow;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.OrangeRed;
+                        result = false;
+                    }
+                }
+            }
+
+            if (!result)
+            {
+                MessageBox.Show(this, "快点来看！快点来看！快点来看！", "有问题快修！", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
